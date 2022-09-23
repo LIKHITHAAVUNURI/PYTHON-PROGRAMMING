f26b80511a501cbba52523a6678baf7429e9ffd6
likhitha <likhithalucky8436@gmail.com>
2022-09-23 20:09:49 +0530

    message

---
+++ logical.py
@@ -0,0 +1,13 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=21.3
+>>> b=31.4
+>>> c=51.3
+>>> a>b and a>c
+False
+>>> a>=b and a>=c
+False
+>>> a>c or a>b
+False
+>>> a>=b or a<=c
+True
