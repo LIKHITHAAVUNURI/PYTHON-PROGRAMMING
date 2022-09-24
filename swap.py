29eb2f1cc4fd5e077a17a920d8d7021a1c4093b8
likhitha <likhithalucky8436@gmail.com>
2022-09-24 11:11:38 +0530

    <message>

---
+++ swap.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number:-"))
+b=int(input("enter the number:-"))
+print("values before swapping")
+print("values before swapping:-");
+print(a)
+print(b)
+a=a+b
+b=a-b
+a=a-b
+print("values after swapping:-");
+print(a)
+print(b)
